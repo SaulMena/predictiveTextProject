3a9074cad07aeaed431dd79673ee026b567812aa
Saul Mena <elianmena550@gmail.com>
2024-01-25 19:45:32 -0600

    CSS/JS corrections

--- app.js
+++ app.js
@@ -2,29 +2,36 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
-const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak']
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 
+'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 
+'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 
+'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 
+'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 
+'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 
+'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 
+'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 
+'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 
+'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 
+'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 
+'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 
+'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak']
 
 function search(str) {
   let results = [];
-
-  // Filter the fruit list based on user input
   results = fruit.filter(fruit => fruit.toLowerCase().includes(str.toLowerCase()));
-
   return results;
 }
 
 function searchHandler(e) {
   const inputVal = e.target.value.trim();
   const results = search(inputVal);
-
-  // Show the suggestions in the dropdown
   showSuggestions(results, inputVal);
 }
 
 function showSuggestions(results, inputVal) {
   suggestions.innerHTML = '';
 
-  if (results.length > 0) {
+  if (inputVal.length > 0) {
     results.forEach(result => {
       const li = document.createElement('li');
       li.textContent = result;
@@ -33,6 +40,7 @@ function showSuggestions(results, inputVal) {
 
     suggestions.classList.add('has-suggestions');
   } else {
+    suggestions.appendChild("");
     suggestions.classList.remove('has-suggestions');
   }
 }
